3f17ac22975dbb26f04ea7c588cc1c49947b5522
Luke <luke@Lukes-MacBook-Pro.local>
2018-09-21 09:53:34 -0700

    started styling feed

--- petpals_app/admin.py
+++ petpals_app/admin.py
@@ -3,8 +3,5 @@ from . import models
 
 admin.site.register(models.UserProfileInfo)
 admin.site.register(models.Post)
-<<<<<<< HEAD
 admin.site.register(models.Like)
-=======
 admin.site.register(models.Comment)
->>>>>>> f7940206b4c691e044779d86e27e19918f705150
