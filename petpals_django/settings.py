19de22138e351a332e19f5ae5324535ab9bee4b4
natalie-poulson <natalieplsn@gmail.com>
2018-09-21 13:56:58 -0700

    fixed merge conflicts

--- petpals_django/settings.py
+++ petpals_django/settings.py
@@ -138,5 +138,5 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'))
+STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static')),
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
